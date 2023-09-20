20a8e3270d21a2b1fc4154a96cc697237035b9b2
Oghenetega Esedere <arcteggzz@gmail.com>
2023-09-20 10:53:27 +0100

    feat:update base setup

--- src/pages/HomePage/Components/Hero.tsx
+++ src/pages/HomePage/Components/Hero.tsx
@@ -1,19 +1,7 @@
 import styles from "../Styles/Hero.module.scss";
 
 const Hero = () => {
-  return (
-    <header className={styles.Hero}>
-      <p>Tega</p>
-      <h1>Oghenetega Esedere</h1>
-      <h4>Fullstack Web Developer</h4>
-      <div className={styles.blinds_cover}></div>
-
-      <div className={styles.mouse_scroll_container}>
-        <p>Images</p>
-        <p>scroll down</p>
-      </div>
-    </header>
-  );
+  return <header className={styles.Hero}>Tega</header>;
 };
 
 export default Hero;
